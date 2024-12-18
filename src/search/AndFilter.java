5930bdb917d43e08b7943da21deafd0fb4503bf8
Konstantin Nikolov <kosi691@icloud.com>
2024-12-18 22:06:13 +0200

    Add AndFilter

---
+++ src/search/AndFilter.java
@@ -0,0 +1,21 @@
+package search;
+
+import java.util.List;
+
+public class AndFilter<T> implements Filter<T> {
+    private final List<Filter<T>> filters;
+
+    public AndFilter(List<Filter<T>> filters) {
+        this.filters = filters;
+    }
+
+    @Override
+    public boolean matches(T item) {
+        for (Filter<T> filter : filters) {
+            if (!filter.matches(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
